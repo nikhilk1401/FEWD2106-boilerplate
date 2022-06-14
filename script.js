cb0022f4a7902062aac68ffd82389945bc52cac7
nikhilk1401 <106423750+nikhilk1401@users.noreply.github.com>
2022-06-14 10:54:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var y = Math.floor(Math.random() * 10 + 1);
+      
+    // counting the number of guesses
+    // made for correct Guess
+        
+   // number guessed by user     
+  
+   if() /* if guessed number equal the actual number*/
+   {    
+       alert("CONGRATULATIONS!!! YOU GUESSED IT RIGHT IN "+ guess + " GUESS ");
+   }
+   else if() /* if guessed number is greater than actual number*/ 
+   {    
+       alert("OOPS SORRY!! TRY A SMALLER NUMBER");
+   }
+   else
+   {
+       alert("OOPS SORRY!! TRY A GREATER NUMBER")
+   }
+}
